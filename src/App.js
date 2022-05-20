2c6c025e1b4ee0e2a736fa0cd17b4b2ae4936d77
SabrinaAfroz1 <sabrina1234091@gmail.com>
2022-05-21 01:45:21 +0600

    add rest

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import Dashboard from './Pages/Dashboard/Dashboard';
 import MyAppointment from './Pages/Dashboard/MyAppointment';
 import MyReview from './Pages/Dashboard/MyReview';
+import MyHistory from './Pages/Dashboard/MyHistory';
 
 function App() {
   return (
@@ -29,23 +30,12 @@ function App() {
         </RequireAuth>}></Route>
 
         <Route path='dashboard' element={<RequireAuth>
-          <Dashboard>
-
-
-
-          </Dashboard>
-        </RequireAuth>}>
+          <Dashboard></Dashboard></RequireAuth>}>
 
           <Route index element={<MyAppointment></MyAppointment>}></Route>
           <Route path='review' element={<MyReview></MyReview>}></Route>
-
-
-
-
+          <Route path='history' element={<MyHistory></MyHistory>}></Route>
         </Route>
-
-
-
 
 
         <Route path='/login' element={<Login></Login>}></Route>
